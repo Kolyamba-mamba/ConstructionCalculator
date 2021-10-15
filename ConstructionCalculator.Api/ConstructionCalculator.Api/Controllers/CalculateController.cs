9373c15c81ca67b03790fd5457331e9c2bbc6b65
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-10-15 18:12:12 +0300

    #rm1 создал контроллер

---
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Controllers/CalculateController.cs
@@ -0,0 +1,28 @@
+﻿using ConstructionCalculator.Api.Helpers;
+using ConstructionCalculator.Api.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionCalculator.Api.Controllers
+{
+    [Route("[controller]")]
+    public class CalculateController : Controller
+    {
+        [Route("calculatewall")]
+        [HttpPost]
+        public async Task<IActionResult> CalculateWall([FromBody] InputNumbersDto inputNumbers)
+        {
+            if (inputNumbers == null)
+                return BadRequest("The input data is null");
+
+            var h = HelperDeterminationDepthOfTheRetainingWallBelowBottomOfPit
+                .GetDepthOfSealingOfRetainingWallBelowTheBottomOfPit(inputNumbers);
+
+
+            return Ok(h);
+        }
+    }
+}
