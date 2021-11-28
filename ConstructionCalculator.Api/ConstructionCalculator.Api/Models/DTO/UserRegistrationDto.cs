aa77ad829f49360a21f4497b1456f770d981d98b
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-11-28 19:03:07 +0300

    Добавил дто для регистрации и входа

---
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Models/DTO/UserRegistrationDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionCalculator.Api.Models.DTO
+{
+    public class UserRegistrationDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+    }
+}
