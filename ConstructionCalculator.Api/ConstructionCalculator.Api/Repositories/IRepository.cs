f8870c19a44d3a8cb20020006992eedb0f6bc60c
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-11-28 19:02:12 +0300

    Создал crud репозиторий

---
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionCalculator.Api.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        // Получение списка
+        IQueryable<T> Get();
+        //Получение одного элемента
+        Task<T> GetById(Guid id);
+        //Изменение элемента
+        Task Update(T item);
+        //Удаление элемента
+        Task Delete(Guid id);
+        //Создание элемента
+        Task Create(T item);
+    }
+}
