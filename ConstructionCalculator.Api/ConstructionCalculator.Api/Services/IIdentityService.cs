f1276dc0bb8051498b6aacfd503b0ca302f8beb7
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-11-28 19:03:40 +0300

    Создал интерфейс авторизации

---
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Services/IIdentityService.cs
@@ -0,0 +1,11 @@
+﻿using ConstructionCalculator.Api.Models;
+using ConstructionCalculator.Api.Repositories;
+using System.Threading.Tasks;
+
+namespace ConstructionCalculator.Api.Services
+{
+    public interface IIdentityService
+    {
+        Task<AuthResult> AuthenticationUserAsunc(string login, string password, IRepository<User> repository);
+    }
+}
