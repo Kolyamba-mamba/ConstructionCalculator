b6233ea1da34bc3380b96bf333730cad1e29d29d
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-11-28 19:01:08 +0300

    Добавил работу с БД

---
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Helpers/DatabaseHelpers/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using ConstructionCalculator.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConstructionCalculator.Api.Helpers.DatabaseHelpers
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Helpers/SampleData.cs
@@ -0,0 +1,34 @@
+﻿using ConstructionCalculator.Api.Helpers.DatabaseHelpers;
+using ConstructionCalculator.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionCalculator.Api.Helpers
+{
+    public class SampleData
+    {
+        public static void Initialize(ApplicationContext context)
+        {
+            if (!context.Users.Any())
+            {
+                context.Users.AddRange(
+                    new User
+                    {
+                        Email = "admin@admin.com",
+                        Password = "1234",
+                        UserName = "admin"
+                    },
+                    new User
+                    {
+                        Email = "admin2@admin.com",
+                        Password = "1234",
+                        UserName = "admin2"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
