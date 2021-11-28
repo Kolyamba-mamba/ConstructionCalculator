3329bb70e18027c0d281b0abfd2f09700852b6f5
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-11-28 19:05:15 +0300

    Вспомогательные классы для авторизации

---
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Models/AuthResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConstructionCalculator.Api.Models
+{
+    public class AuthResult
+    {
+        public bool IsSuccess { get; set; }
+        public string Token { get; set; }
+    }
+}
