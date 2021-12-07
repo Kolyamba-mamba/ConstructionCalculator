2e4be1ea9f22a32efb70de260be4aea2dceb43cc
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-12-07 22:46:04 +0300

    маппинг

---
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Mapping/CalculateEntityAutomapperConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ConstructionCalculator.Api.Models;
+using ConstructionCalculator.Api.Models.DTO;
+
+namespace ConstructionCalculator.Api.Mapping
+{
+    public class CalculateEntityAutomapperConfiguration
+    {
+        public CalculateEntityAutomapperConfiguration(IMapperConfigurationExpression cfg) 
+        {
+            cfg.CreateMap<CalculateEntity, CalculateEntityDto>();
+        }
+    }
+}
