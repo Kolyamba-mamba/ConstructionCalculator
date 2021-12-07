39405b6af8a0491d6d2840743686d7e787e0655c
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2021-12-07 22:46:19 +0300

    маппинг

--- ConstructionCalculator.Api/ConstructionCalculator.Api/Mapping/AutomapperConfigurationStorage.cs
+++ ConstructionCalculator.Api/ConstructionCalculator.Api/Mapping/AutomapperConfigurationStorage.cs
@@ -11,6 +11,7 @@ namespace ConstructionCalculator.Api.Mapping
             Configuration = new MapperConfiguration(cfg =>
             {
                 new RegistrationAutomapperConfiguration(cfg);
+                new CalculateEntityAutomapperConfiguration(cfg);
             });
         }
     }
